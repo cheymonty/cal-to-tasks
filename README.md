df66641f45943c5de39090354fe0fdac8a6a738d
cheymonty <45781686+cheymonty@users.noreply.github.com>
2021-02-17 16:25:38 -0500

    updated readme with instructions

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# Google Calendar to Google Tasks
+# Google Calendar Events to Google Tasks
+*This project moves the calendar events into the default task list*
+
+* Download this repository and save all the files into a folder
+
+* Find the Calendar ID of the calendar you want to move to tasks (Settings of the calendar -> Calendar ID)
+
+* Go to the [Google Calendar Api Website](https://developers.google.com/calendar/quickstart/python) and find the **Enable the Google Calendar API** button. Follow the steps and save the .json file as `credentialsCal.json` in your working folder
+
+* Go to the [Google Tasks Api Website](https://developers.google.com/tasks/quickstart/python) and find the **Enable the Google Tasks API** button. Follow the steps and save the .json as `credentialsTask.json` in your working folder
+
+* Run `pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib` in the working folder
+
+* Run `python3 driver.py` in the working folder
+
+* Enter the calendar ID when prompted
+
--- driver.py
+++ driver.py
@@ -6,22 +6,18 @@ from googleapiclient.discovery import build
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 
-# If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar', 'https://www.googleapis.com/auth/tasks']
 
 
 def main():
-    # Shows basic usage of the Google Calendar API.
     calendar_id = input("Enter calendar ID of Google Calendar you would like to move to Tasks: ")
 
     creds = None
-    # The file token.pickle stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
+  
     if os.path.exists('tokenCal.pickle'):
         with open('tokenCal.pickle', 'rb') as token:
             creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
+
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -29,13 +25,13 @@ def main():
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentialsCal.json', SCOPES[0])
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
+        
         with open('tokenCal.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('calendar', 'v3', credentials=creds)
 
-    # Call the Calendar API
+    
     now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
     print('Gets all future events from this calendar')
     events_result = service.events().list(calendarId=calendar_id, timeMin=now,
@@ -63,7 +59,7 @@ def cal_to_tasks(events):
     if os.path.exists('tokenTask.pickle'):
         with open('tokenTask.pickle', 'rb') as token:
             creds = pickle.load(token)
-    # If there are no (valid) credentials available, let the user log in.
+    
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
@@ -71,13 +67,13 @@ def cal_to_tasks(events):
             flow = InstalledAppFlow.from_client_secrets_file(
                 'credentialsTask.json', SCOPES[1])
             creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
+       
         with open('tokenTask.pickle', 'wb') as token:
             pickle.dump(creds, token)
 
     service = build('tasks', 'v1', credentials=creds)
 
-    # Call the Tasks API
+    
     results = service.tasklists().list(maxResults=10).execute()
     task_lists = results.get('items', [])
 
@@ -90,7 +86,6 @@ def cal_to_tasks(events):
         due = event['start'].get('dateTime')
         if due == None:
             due = event['start'].get('date') + "T09:00:00-05:00"
-        # print(due)
       
         request_body = {
             'title': title,
